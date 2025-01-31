2008634f5239efa1243227d08c0231dee67f9965
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2025-01-31 21:28:59 +0700

    Add UI Asset, font (#3)
    * Add UI Asset
    
    * Add font and setup quest UI
    
    * WIP
    
    * Add New font for level text
    
    * Add UI Game Asset

--- Assets/Scripts/UI/PuzzleQuestUI.cs
+++ Assets/Scripts/UI/PuzzleQuestUI.cs
@@ -1,23 +1,26 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleQuestUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI itemNameText;
     [SerializeField] private TextMeshProUGUI itemCountText;
-    
+    [SerializeField] private Image icon;
+    [SerializeField] private Image iconShadow;
+
     public void BindingUI(PuzzleQuest puzzleQuest)
     {
         itemNameText.text = $"ID: {puzzleQuest.ItemID}";
         UpdateCount(puzzleQuest.TargetQuantity);
-        
+
         puzzleQuest.OnUpdateItemCount = UpdateCount;
         puzzleQuest.OnCompleteQuest = OnCompleteQuest;
     }
 
     private void UpdateCount(int targetQuantity)
     {
-        itemCountText.text = $"x{targetQuantity}";
+        itemCountText.text = "x" + targetQuantity;
     }
 
     private void OnCompleteQuest()
