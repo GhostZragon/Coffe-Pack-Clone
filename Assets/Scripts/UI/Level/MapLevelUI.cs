8cb852e6682f2b9d783c98cfc8012c04e521dd6d
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-02-03 19:29:07 +0700

    WIP

--- Assets/Scripts/UI/Level/MapLevelUI.cs
+++ Assets/Scripts/UI/Level/MapLevelUI.cs
@@ -8,12 +8,26 @@ public class MapLevelUI : MonoBehaviour
 
     [SerializeField] private int mapIndex;
     
+    
+    
     [Button]
-    private void ActiveMap()
+    private void ActiveMap(int startLevel)
     {
         for (int i = 0; i < MapImages.Length; i++)
         {
             MapImages[i].gameObject.SetActive(i == mapIndex);
+
+            if (i == mapIndex)
+            {
+                // load all button
+
+                var buttons = MapImages[i].transform.GetComponentsInChildren<LevelUI>();
+
+                foreach (var button in buttons)
+                {
+                    button.Setup(startLevel++,false);
+                }
+            }
         }
     }
 }
